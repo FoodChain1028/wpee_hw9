2f72cbb98895622ebab832ac20772ce2216dfd2e
FoodChain1028 <sh1001309@gmail.com>
2022-12-26 02:08:30 +0800

    x

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API_ROOT =  
   process.env.NODE_ENV === "production" 
-    ? "http://localhost:4001/api"
+    ? "/api"
     : "http://localhost:4001/api";
 
 const api = axios.create({ baseURL: API_ROOT });
