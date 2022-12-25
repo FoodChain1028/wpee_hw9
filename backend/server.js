94b11389197e1b9c7abd18ec2de814fc17bb3eec
FoodChain1028 <sh1001309@gmail.com>
2022-12-26 03:15:32 +0800

    x

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import path from "path";
 import routes from './src/routes';
 import db from './src/db';
 
@@ -14,6 +15,14 @@ app.get('/', (req, res) => {
   console.log(res)
   res.send('Hello, World!').status(200);
 });
+
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "../frontend", "build")));
+  app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
+  });
+}
 
 app.listen(port, () =>
  console.log(`Example app listening on port ${port}!`),
