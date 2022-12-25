a17bbec059ca74e6fded78338f7234aa39eb2d98
FoodChain1028 <sh1001309@gmail.com>
2022-12-26 03:27:56 +0800

    x

--- backend/server.js
+++ backend/server.js
@@ -6,28 +6,20 @@ import db from './src/db';
 
 const app = express();
 const port = process.env.PORT || 4001;
+const __dirname = path.resolve();
+
 
 // init middleware
 app.use(cors());
-app.use(express.json());
-
-app.get('/api', (req, res) => {
-  console.log(res)
-  res.send('Hello, World!').status(200);
+app.use('/api', routes);
+app.use(express.static(path.join(__dirname, "../frontend", "build")));
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
 });
 
-if (process.env.NODE_ENV === "production") {
-  const __dirname = path.resolve();
-  app.use(express.static(path.join(__dirname, "../frontend", "build")));
-  app.get("/*", function (req, res) {
-    res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
-  });
-}
+//connect db
+db.connect();
 
 app.listen(port, () =>
  console.log(`Example app listening on port ${port}!`),
 );
-
-//connect db
-db.connect();
-app.use('/api', routes);
